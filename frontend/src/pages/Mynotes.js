61fb66bb172b5539f19ddceb929fd57c6afa36e1
jequex <onyejekwe.harddisk20@gmail.com>
2021-07-13 20:24:35 +0100

    scaffolding done

---
+++ frontend/src/pages/Mynotes.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Mynotes = () => {
+    return (
+        <div>
+            my notes
+        </div>
+    )
+}
+
+export default Mynotes;
--- frontend/src/styled_components/GlobalStyle.js
+++ frontend/src/styled_components/GlobalStyle.js
@@ -14,5 +14,26 @@ body, html {
     width: 90%;
 }
 
+body{
+    font-family: -apple-system, BlinkMacSystemFont, 'Segoe UI', Roboto,
+    Oxygen-Sans, Ubuntu, Cantarell, 'Helvetica Neue', sans-serif;
+    background-color: #fff;
+    line-height: 1.4;
+}
+
+a:link,
+a:visited {
+    color: #0077cc;
+}
+
+a:hover,
+a:focus {
+color: #004499;
+}
+
+code,
+pre {
+max-width: 100%;
+}
 
 `;
